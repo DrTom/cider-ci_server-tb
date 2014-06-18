05dbabd8014f98bc4df933c596eeeb3f637bd4a2
Thomas Schank <DrTom@schank.ch>
2014-06-18 15:06:55 +0200

    Add initial radiator config

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,14 @@ Definition.create name: "All tests" ,
 
 executor=Executor.find_or_initialize_by(name: "Localhost")
 
+executor.update_attributes!(
+  host: "127.0.0.1",
+  port: "8443",
+  ssl: true
+)
+
+executor.reload
+
 traits= (executor.traits || []).concat([
           'firefox',
           'imagemagick',
@@ -33,17 +41,7 @@ traits= (executor.traits || []).concat([
           'tightvnc',
         ]).sort.uniq
 
-
-executor.update_attributes!(
-  host: "127.0.0.1",
-  port: "8443",
-  traits: traits,
-  ssl: true
-)
-
-executor.save!
-
-
+executor.reload.update_attributes! traits: traits
 
 repo= Repository.find_or_initialize_by name: "Cider-CI Bash Demo Project"
 repo.update_attributes! \
@@ -56,27 +54,18 @@ if welcome_page_settings.welcome_message.blank?
   welcome_page_settings.update_attributes! \
     welcome_message: "# Welcome to your installation of Cider-CI"
 end
+
   
 if welcome_page_settings.radiator_config.blank? 
+  radiator_config= YAML.load_file \
+    Rails.root.join("db", "initial_radiator_config.yml")
   welcome_page_settings.update_attributes! \
-    radiator_config:  YAML.load(%<
-      ---
-      rows:
-      - name: Bash Demo Project
-        items:
-        - repository_name: Cider-CI Bash Demo Project
-          branch_name: master
-          definition_name: All tests
-        - repository_name: Cider-CI Bash Demo Project
-          branch_name: wip
-          definition_name: All tests
-        - repository_name: No repository
-          branch_name: nix
-          definition_name: No tests
-    >)
-
+    radiator_config: radiator_config
 end
   
+
+
+
 
 
 
